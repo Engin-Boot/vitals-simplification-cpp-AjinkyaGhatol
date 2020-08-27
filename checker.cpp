52c6cda00b7c9aca5321ae224734b16ac59cc8ce
Ajinkya Ghatol <ajinkyas.ghatol@gmail.com>
2020-08-27 16:33:31 +0530

    update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -2,8 +2,8 @@
 enum { bpm, spo2, respRate };
 
 
-int limit[3][2] = { {70,150},{90,1000} ,{30,95}};
-bool isOk(int vital,int val)
+int limit[3][2] = { {70,150},{90,1000} ,{30,95} };
+bool isOk(int vital, int val)
 {
     if (val < limit[vital][0] || val > limit[vital][1]) {
         return false;
@@ -12,17 +12,14 @@ bool isOk(int vital,int val)
 }
 
 int main() {
-    
+
     assert(isOk(bpm, 80) == true);
     assert(isOk(bpm, 160) == false);
 
     assert(isOk(spo2, 95) == true);
     assert(isOk(spo2, 90) == true);
-    
+    assert(isOk(spo2, 89) == false);
+
     assert(isOk(respRate, 40) == true);
     assert(isOk(respRate, 100) == false);
-       
-    
-    
-
 }
