b58a50914447340cd41de06aab6b0e1d460713ec
Ajinkya Ghatol <ajinkyas.ghatol@gmail.com>
2020-08-28 13:41:28 +0530

    update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -1,25 +1,34 @@
 #include <assert.h>
-enum { bpm, spo2, respRate };
+#include<iostream>
+using namespace std;
 
-
-int limit[3][2] = { {70,150},{90,1000} ,{30,95} };
-bool isOk(int vital, int val)
+struct vital
 {
-    if (val < limit[vital][0] || val > limit[vital][1]) {
+    float minLimit;
+    float maxLimit;
+};
+
+vital BPM { 70, 150 }, SPO2{ 90,100 }, RESPRATE{30,95};
+
+bool isOk(vital vital_passed, float val)
+{
+    if (val < vital_passed.minLimit || val > vital_passed.maxLimit) {
         return false;
     }
     return true;
 }
 
 int main() {
+    
+    assert(isOk(BPM, 80) == true);
+    assert(isOk(BPM, 160) == false);
 
-    assert(isOk(bpm, 80) == true);
-    assert(isOk(bpm, 160) == false);
+    assert(isOk(SPO2, 95) == true);
+    assert(isOk(SPO2, 90) == true);
+    assert(isOk(SPO2, 89) == false);
 
-    assert(isOk(spo2, 95) == true);
-    assert(isOk(spo2, 90) == true);
-    assert(isOk(spo2, 89) == false);
+    assert(isOk(RESPRATE, 40) == true);
+    assert(isOk(RESPRATE, 100) == false);
 
-    assert(isOk(respRate, 40) == true);
-    assert(isOk(respRate, 100) == false);
+    return 0;
 }
